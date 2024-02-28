3f731385bbe57fdf81588a6d585c6b441d6922bc
Bruno Silva <brunosilvabrait@gmail.com>
2024-02-28 17:42:38 -0300

    Configure routes to CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts, only: %i[index create update destroy show]
+
+  resources :comments, only: %i[create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
