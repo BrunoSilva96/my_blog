6fd0b7f4d423947b5aad947036cbe845b0214160
Bruno Silva <brunosilvabrait@gmail.com>
2024-03-21 19:07:53 -0300

    Correcting word association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   VALID_EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :email, presence: true, length: { maximum: 260 }, format: { with: VALID_EMAIL_FORMAT },
                     uniqueness: { case_sensitive: false }
-  has_many :post
-  has_many :comment
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
